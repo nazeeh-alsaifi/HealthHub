d6073e655aa586df4080405923ef69527bbd8daa
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-03 19:53:23 +0300

    changing the messages

--- database/seeds/MessageSeeder.php
+++ database/seeds/MessageSeeder.php
@@ -17,12 +17,12 @@ class MessageSeeder extends Seeder
         DB::table('messages')->insert([
             'sent_by' => 2,
             'received_by' => 1,
-            'subject' => 'heart disease',
-            'body' => 'hey doc, i had a severe heart attack when my girlfriend broke up with me KEKW :D',
+            'subject' => 'Do I need to lose or gain weight for my health?',
+            'body' => 'So my weight has been increasing since last year, Even so, I don\'t eat that much and most of my meals are healthy and organic. Also, I start feeling like my body is tired all the time and recently started to feel some pain in my hear.what should I do, Please?',
             'phone_num' => '134134',
             'age' => 25,
             'sex' => 'male',
-            'status' => '1',
+            'status' => '0',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
@@ -30,12 +30,12 @@ class MessageSeeder extends Seeder
         DB::table('messages')->insert([
             'sent_by' => 3,
             'received_by' => 1,
-            'subject' => 'heart attack incoming',
-            'body' => "hey doc, i think i'm going to have a severe heart attack if my grandmother shouted at me KEKW :D",
+            'subject' => 'Am I at risk for stroke?',
+            'body' => " I am feeling some pain in my heart when I do some small exercises. Should I go to the doctor and get diagnose or it is a normal thing happen to people.",
             'phone_num' => '567567',
             'age' => 24,
             'sex' => 'male',
-            'status' => '1',
+            'status' => '0',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
@@ -43,8 +43,8 @@ class MessageSeeder extends Seeder
         DB::table('messages')->insert([
             'sent_by' => 1,
             'received_by' => 2,
-            'subject' => 'heart disease',
-            'body' => 'dear nazeeh, thank you for writing to us, i think you are going to be just fine :D',
+            'subject' => 'Do I need to lose or gain weight for my health?',
+            'body' => 'Definitely, I recommend you to see a doctor and do some tests on your Thyroid, maybe you have some hormone issues.',
             'status' => '1',
             'reply_on' => '1',
             'created_at' => now(),
@@ -54,18 +54,18 @@ class MessageSeeder extends Seeder
         DB::table('messages')->insert([
             'sent_by' => 1,
             'received_by' => 3,
-            'subject' => 'heart attack incoming',
-            'body' => 'dear suliman, thank you for writing to us, i think you should shout back at her .lol :D',
+            'subject' => 'Am I at risk for stroke?',
+            'body' => 'I recommend you do some blood tests and after that, you can see a doctor and give it to him and he will guide you to what you should do. ',
             'status' => '1',
             'reply_on' => '2',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         DB::table('messages')->insert([
-            'sent_by' => 2,
+            'sent_by' => 3,
             'received_by' => 1,
-            'subject' => 'headache',
-            'body' => 'hey doc, i had a severe headache when i am not having enough sleep  :{',
+            'subject' => 'What kind of physical activity is right for me?',
+            'body' => 'I am 56 years old, and I love being active and do exercises to re-fill my energy. However, sometimes I feel some pain in my back and shoulder. Should you give me some good advice on what is the best exercise that I can do without hearting my body.',
             'phone_num' => '567567',
             'age' => 24,
             'sex' => 'male',
@@ -75,10 +75,10 @@ class MessageSeeder extends Seeder
         ]);
 
         DB::table('messages')->insert([
-            'sent_by' => 2,
+            'sent_by' => 3,
             'received_by' => 1,
-            'subject' => 'blood test',
-            'body' => "hey doc, i'm going to have a blood test",
+            'subject' => 'Fears of heart diseases',
+            'body' => "Based on my history and risk factors, what can I do to lower my risk of heart disease and stroke?",
             'phone_num' => '567567',
             'age' => 24,
             'sex' => 'male',
